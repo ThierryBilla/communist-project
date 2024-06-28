11617276f2cbc90600da13f9d2c20e39c9c92d65
Thierry Billa <thierry.billa96@gmail.com>
2024-06-28 11:10:25 +0200

    bouton get started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../css/Hero.module.css';
 import communistHeroImage from '../assets/picture/communist-hero.jpg';
+import getstarted from '../assets/picture/logo-getstarted.png';
 
 const Hero = () => {
+    const navigate = useNavigate();
+
+    const handleGetStartedClick = () => {
+        navigate('/signin');
+    };
+
     return (
         <div className={styles.hero}>
             <img src={communistHeroImage} alt="Hero Image" className={styles['hero-image']} />
-            <div className={styles.overlay}></div> {/* Ajoutez l'overlay ici */}
+            <div className={styles.overlay}></div>
             <div className={styles['button-container']}>
-                <button className={styles['get-started-button']}>GET STARTED</button>
+                <button className={styles['get-started-button']} onClick={handleGetStartedClick}>
+                    <img src={getstarted} alt="Get Started" className={styles['get-started-image']} />
+                </button>
             </div>
         </div>
     );
